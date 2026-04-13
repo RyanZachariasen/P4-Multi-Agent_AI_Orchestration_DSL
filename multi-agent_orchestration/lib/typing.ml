e702be13f0c9702dac1cb1e740273db08d7fa05b
asbjorngosvig <asbjorn.gosvig@gmail.com>
2026-04-13 14:01:39 +0200

    error handling in typing.ml (unbound variables, resource required on function calls, bad arity and type mismatching)

--- multi-agent_orchestration/lib/typing.ml
+++ multi-agent_orchestration/lib/typing.ml
@@ -10,3 +10,23 @@ let string_of_typ = function
   | TFloat   -> "Float"
   | TNamed s -> s
 
+
+(* Error handling*)
+exception Type_error of loc option * string
+let error ?loc msg = raise (Type_error (loc, msg))
+
+let unbound_var         ?loc x              = error ?loc ("Unbound variable: " ^ x)
+let unbound_fun         ?loc f              = error ?loc ("Unbound function: " ^ f)
+let unbound_type        ?loc t              = error ?loc ("Unbound type: " ^ t)
+let unbound_resource    ?loc r              = error ?loc ("Unbound resource: " ^ r)
+let unbound_field       ?loc s f            = error ?loc ("Unbound field: Type " ^ s ^ " has no field " ^ f)
+let resource_required   ?loc f              = error ?loc ("Resource required: Call to " ^ f ^ " requires a resource")
+
+let bad_arity ?loc f expected got =
+  error ?loc ("Bad arity: function " ^ f ^ " expects " ^
+              string_of_int expected ^ " arguments, got " ^
+              string_of_int got)
+
+let type_mismatch ?loc t1 t2 =
+  error ?loc ("Type mismatch: expected " ^ string_of_typ t2 ^
+              ", got " ^ string_of_typ t1)
