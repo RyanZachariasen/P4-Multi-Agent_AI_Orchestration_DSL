cba1c175cc7bbd18bb9c76ce41c76c068f2b6e8a
nichzar <nichzar19@gmail.com>
2026-04-29 22:21:22 +0200

    Fixed main to recognize parse/type only tags and test to reflect parser

--- multi-agent_orchestration/bin/main.ml
+++ multi-agent_orchestration/bin/main.ml
@@ -1,45 +1,44 @@
 open Multi_agent_orchestration
 open Lexer
-let handle_filename_given () : string  = 
-  Printf.printf "Using %s \n" Sys.argv.(1);
-  Sys.argv.(1);;
 
-let handle_no_filename_given () : string = 
-  Printf.eprintf "No file given, trying main.mai\n";
-  "main.mai";;
+let parse_only = ref false
+let type_only = ref false
+let ifile = ref ""
 
-let parse_file (filename : string) (lexbuf) = 
-      let _ast = Parser.program Lexer.next_token lexbuf in
-      Printf.printf "Successfully parsed: %s\n" filename;;
+let set_file s = ifile := s
 
+let options =
+  [ "--parse-only", Arg.Set parse_only, "  stops after parsing";
+    "--type-only",  Arg.Set type_only,  "  stops after typing";
+  ]
 
+let usage = "usage: mai [options] file.mai"
 
 let handle_lexer_error (error_message : string) (lexbuf: Lexing.lexbuf) = 
   let pos = lexbuf.lex_curr_p in
   Printf.eprintf "Lexical error at %s:%d:%d: %s\n"
-  pos.pos_fname pos.pos_lnum (pos.pos_cnum - pos.pos_bol) error_message;;
+  pos.pos_fname pos.pos_lnum (pos.pos_cnum - pos.pos_bol) error_message
 
 let handle_parser_error (lexbuf: Lexing.lexbuf) = 
-    let pos = lexbuf.lex_curr_p in
-    Printf.eprintf "Syntax error at %s:%d:%d\n" 
-      pos.pos_fname pos.pos_lnum (pos.pos_cnum - pos.pos_bol);
-    exit 1
+  let pos = lexbuf.lex_curr_p in
+  Printf.eprintf "Syntax error at %s:%d:%d\n" 
+    pos.pos_fname pos.pos_lnum (pos.pos_cnum - pos.pos_bol);
+  exit 1
 
 let () =
-    let filename = (if Array.length Sys.argv > 0 then handle_filename_given() else handle_no_filename_given()) in
-    let file_channel = open_in filename in
-    let lexbuf = Lexing.from_channel file_channel in
-    
-
-    lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = filename };
-    
-
-    try
-      parse_file filename lexbuf;
-      close_in file_channel
-    with
-      | Lexer.Lexing_error error_message ->
-        handle_lexer_error error_message lexbuf
-      | Parser.Error ->
-        handle_parser_error lexbuf
-    
+  Arg.parse options set_file usage;
+  if !ifile = "" then begin Printf.eprintf "No file given\n"; exit 1 end;
+  let file_channel = open_in !ifile in
+  let lexbuf = Lexing.from_channel file_channel in
+  lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = !ifile };
+  try
+    let _ast = Parser.program Lexer.next_token lexbuf in
+    close_in file_channel;
+    if !parse_only then exit 0;
+    (* TODO: call Typing.program here *)
+    if !type_only then exit 0;
+  with
+  | Lexer.Lexing_error error_message ->
+    handle_lexer_error error_message lexbuf
+  | Parser.Error ->
+    handle_parser_error lexbuf
