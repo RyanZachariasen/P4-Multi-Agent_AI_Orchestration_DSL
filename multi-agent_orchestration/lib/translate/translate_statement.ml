c82289c76f6152b09edfc203a7e3fd90f6597b35
RyanZachariasen <143599102+RyanZachariasen@users.noreply.github.com>
2026-05-09 18:01:58 +0200

    Translate SLet to PyAssign in translate_statement

--- multi-agent_orchestration/lib/translate/translate_statement.ml
+++ multi-agent_orchestration/lib/translate/translate_statement.ml
@@ -1,6 +1,6 @@
 let statement (stmt: Typed_ast.statement) : Py_ast.py_statement = 
   match stmt with
-  | Typed_ast.SLet (name, typ, expr) -> raise Not_found
+  | Typed_ast.SLet (name, typ, expr) -> Py_ast.PyAssign (name, Translate_expr.expr expr)
   | Typed_ast.SPrint expr -> raise Not_found
   | Typed_ast.SReadFile expr -> raise Not_found
   | Typed_ast.SWriteFile (expr_1, expr_2) -> raise Not_found
