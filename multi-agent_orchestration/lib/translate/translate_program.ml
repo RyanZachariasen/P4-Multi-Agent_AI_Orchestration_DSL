95bccd87fca1e3a29f9aa01591caf10c8dbdc608
RohanAtik <hanhan@live.dk>
2026-05-11 17:39:52 +0200

    Add program and workflow translation

--- multi-agent_orchestration/lib/translate/translate_program.ml
+++ multi-agent_orchestration/lib/translate/translate_program.ml
@@ -1,2 +1,9 @@
-let program (prog: Typed_ast.program) : Py_ast.py_module = 
-  raise Not_found
+let program (prog : Typed_ast.program) : Py_ast.py_module =
+  let declarations =
+    List.map Translate_declaration.declaration prog.prog_decls
+  in
+  let workflow_body = Translate_workflow.workflow prog.prog_workflow in
+  {
+    imports = [];
+    body = declarations @ workflow_body;
+  }
--- multi-agent_orchestration/lib/translate/translate_workflow.ml
+++ multi-agent_orchestration/lib/translate/translate_workflow.ml
@@ -1,2 +1,4 @@
-let workflow (wf: Typed_ast.workflow) : Py_ast.py_statement list = 
-  raise Not_found
+let workflow (wf : Typed_ast.workflow) : Py_ast.py_statement list =
+  List.map Translate_statement.statement wf.workflow_body
+
+  
