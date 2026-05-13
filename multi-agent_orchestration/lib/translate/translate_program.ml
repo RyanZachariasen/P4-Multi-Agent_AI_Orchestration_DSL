df4bc744db34e158d8a94882de726d76e1fc65df
RohanAtik <hanhan@live.dk>
2026-05-13 12:27:36 +0200

    Python main guard

--- multi-agent_orchestration/lib/translate/translate_program.ml
+++ multi-agent_orchestration/lib/translate/translate_program.ml
@@ -3,7 +3,17 @@ let program (prog : Typed_ast.program) : Py_ast.py_module =
     List.map Translate_declaration.declaration prog.prog_decls
   in
   let workflow_body = Translate_workflow.workflow prog.prog_workflow in
+  let main_guard =
+    Py_ast.PyIf (
+      Py_ast.PyCompare (
+        Py_ast.PyName "__name__",
+        Py_ast.Eq,
+        Py_ast.PyConst (Py_ast.PyString "__main__")
+      ),
+      workflow_body
+    )
+  in
   {
     imports = [];
-    body = declarations @ workflow_body;
+    body = declarations @ [main_guard];
   }
