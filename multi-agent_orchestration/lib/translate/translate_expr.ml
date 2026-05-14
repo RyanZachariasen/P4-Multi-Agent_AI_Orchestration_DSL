b7c9a989f689877e19434adf72d453ff16ab0b8d
pandajakob <jakobm.dev@gmail.com>
2026-05-14 17:10:23 +0200

    undo translate_expr

--- multi-agent_orchestration/lib/translate/translate_expr.ml
+++ multi-agent_orchestration/lib/translate/translate_expr.ml
@@ -11,24 +11,25 @@ and expr_node (node: Typed_ast.expr_node) : Py_ast.py_expr =
     let binop_expr_2 = expr expr_2 in
     let binop_opperation = binop opperation in
     Py_ast.PyBinOp (binop_opperation, binop_expr_1, binop_expr_2)
-
-  
-
-  | Typed_ast.ECall (name, expr_list, on_resource_option) -> 
-    let function_name = Py_ast.PyName name in
-    let ecall_arguments = List.map expr expr_list in
-    begin match on_resource_option with
-    | None -> Py_ast.PyCall(function_name, ecall_arguments, [])
-    | Some resource_name -> 
-        Py_ast.PyCall(function_name, 
-          ecall_arguments @ [Py_ast.PyName resource_name], 
-        [])
-    end
-
+    
+  | Typed_ast.EReadFile path_expr ->
+    let file_arg = expr path_expr in
+    let open_call = Py_ast.PyCall (Py_ast.PyName "open", [file_arg; Py_ast.PyConst (Py_ast.PyString "r")], []) in
+    Py_ast.PyCall (Py_ast.PyAttr (open_call, "read"), [], [])
 
   | Typed_ast.EField (expr_record, flield_name, typ) ->
     let py_expr = expr expr_record in
     Py_ast.PyAttr (py_expr, flield_name)
+
+  | Typed_ast.ECall (name, expr_list, on_resource_option) -> 
+    let function_name = Py_ast.PyName name in
+    let ecall_arguments = List.map expr expr_list in
+    match on_resource_option with
+    | None -> Py_ast.PyCall(function_name, ecall_arguments, [])
+    | Some resource_name -> 
+      let ecall_arguments = ecall_arguments @ [Py_ast.PyName resource_name] in
+      Py_ast.PyCall(function_name, ecall_arguments, [])
+  
 
 and constant (const: Typed_ast.constant) : Py_ast.py_expr = 
   match const with
