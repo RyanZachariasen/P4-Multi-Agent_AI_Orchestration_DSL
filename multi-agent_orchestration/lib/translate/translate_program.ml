3eb8c3a65eb4ecf837953c137f793cd5d8f09867
pandajakob <jakobm.dev@gmail.com>
2026-05-15 12:12:48 +0200

    fix condegen error

--- multi-agent_orchestration/lib/translate/translate_program.ml
+++ multi-agent_orchestration/lib/translate/translate_program.ml
@@ -5,7 +5,7 @@ let program (prog : Typed_ast.program) : Py_ast.py_module =
   let declarations = List.map Translate_declaration.declaration prog.prog_decls in
 
   let workflow_body = Translate_workflow.workflow prog.prog_workflow in
-  Printf.printf "Done!\n%!";
+
   let main_guard =
     Py_ast.PyIf (
       Py_ast.PyCompare (
