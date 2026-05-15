7589ba4445673ec0cfcbdc2615aaf9332d3deed9
pandajakob <jakobm.dev@gmail.com>
2026-05-15 10:22:45 +0200

    remove print statements

--- multi-agent_orchestration/lib/translate/translate_program.ml
+++ multi-agent_orchestration/lib/translate/translate_program.ml
@@ -1,9 +1,9 @@
 let program (prog : Typed_ast.program) : Py_ast.py_module =
-  Printf.printf "Translating imports...\n%!";
+
   let imports = Translate_import.collect_imports prog in
-  Printf.printf "Translating declarations...\n%!";
+
   let declarations = List.map Translate_declaration.declaration prog.prog_decls in
-  Printf.printf "Translating workflow...\n%!";
+
   let workflow_body = Translate_workflow.workflow prog.prog_workflow in
   Printf.printf "Done!\n%!";
   let main_guard =
