74853e2822340cb2bae5aafc3044fb5ac8beee08
pandajakob <jakobm.dev@gmail.com>
2026-05-17 21:31:46 +0200

    fix indent issues

--- multi-agent_orchestration/lib/codegen/codegen_statement.ml
+++ multi-agent_orchestration/lib/codegen/codegen_statement.ml
@@ -13,36 +13,41 @@ let rec statement (stmt: py_statement) (output_file: out_channel) (indent: strin
 
   
   | PyReturn expr ->
+    output_string output_file (indent);
     output_string output_file ("return ");
     Codegen_expr.expression expr output_file;
     output_string output_file ("\n");
 
   | PyFuncDef (name, args, body) ->
+    output_string output_file (indent);
     output_string output_file "def " ;
     output_string output_file (name^" ") ;
     let args_string = String.concat ", " args in
     
     output_string output_file ("(" ^ args_string ^"):\n");
     List.iter (fun s ->
-          output_string output_file (indent^"\t");
           statement s output_file (indent^"\t");
     ) body;
       output_string output_file ("\n");
   
   | PyImport module_name ->
+    output_string output_file (indent);
     output_string output_file ("import" ^ module_name)
 
   | PyImportFrom (module_name, names) ->
+    output_string output_file (indent);
     let names_string = String.concat "," names in
     output_string output_file ("from" ^ module_name  ^ "import" ^ names_string ^"\n")
 
   | PyClassDef (class_name, base_class, fields) ->
+    output_string output_file (indent);
     output_string output_file "class (BaseModel):\n" ;
     List.iter (fun (name, typ) -> 
           output_string output_file (indent ^ "\t" ^ name ^": "^ typ ^ "\n");
     ) fields;
   
   | PyIf (expr, body) -> 
+    output_string output_file (indent);
     output_string output_file ("if ");
     Codegen_expr.expression expr output_file;
     output_string output_file (":\n");
@@ -51,6 +56,7 @@ let rec statement (stmt: py_statement) (output_file: out_channel) (indent: strin
     ) body;
 
   | PyIfElse (expr, if_body, else_body) -> 
+      output_string output_file (indent);
       output_string output_file ("if ");
       Codegen_expr.expression expr output_file;
       output_string output_file (":\n");
@@ -62,7 +68,8 @@ let rec statement (stmt: py_statement) (output_file: out_channel) (indent: strin
           statement s output_file (indent ^ "\t");
       ) else_body;
   | PyWhile (expr, body) ->
-    output_string output_file (indent^"while ");
+    output_string output_file (indent);
+    output_string output_file ("while ");
     Codegen_expr.expression expr output_file;
     output_string output_file (":\n");
     List.iter (fun (s) ->
