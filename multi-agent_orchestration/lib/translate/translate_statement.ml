08974326c755fb0e7a434d9b431c36b2f68bce03
pandajakob <jakobm.dev@gmail.com>
2026-05-21 10:27:13 +0200

    Update translate_statement.ml

--- multi-agent_orchestration/lib/translate/translate_statement.ml
+++ multi-agent_orchestration/lib/translate/translate_statement.ml
@@ -7,16 +7,14 @@ let rec statement (stmt: statement) : py_statement =
   | SPrint expr -> 
     let value = Translate_expr.expr expr in
     PyExpr (PyCall (PyName "print", [value], []))
-    
-  
   | SWriteFile (expr_1, expr_2) -> 
     let file = Translate_expr.expr expr_1 in
     let content_expr = Translate_expr.expr expr_2 in
     let open_call =PyCall (PyName "open", [file; PyConst (PyString "w")], []) in
     let write_call = PyCall (PyAttr(open_call, "write"), [content_expr], []) in
     PyExpr write_call
-  | SWhile (e, body) -> 
+  | SWhile (e, stmt_list) -> 
     let py_expr = Translate_expr.expr e in
-    let py_body = List.map (fun s -> (statement s)) body in
-    Py_ast.PyWhile (py_expr, py_body)
+    let py_stmt_list = List.map (fun stmt -> (statement stmt)) stmt_list in
+    Py_ast.PyWhile (py_expr, py_stmt_list)
     
